2cdacef387389fe0dffc17ba8a01636c20c6e5fa
Flix_botz <99806982+Priyanshu1057@users.noreply.github.com>
2025-10-25 16:31:31 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -365,17 +365,20 @@ async def start(client, message):
                         
                         btn = [
                             [InlineKeyboardButton('🚀 ꜰᴀꜱᴛ ᴅᴏᴡɴʟᴏᴀᴅ / ᴡᴀᴛᴄʜ ᴏɴʟɪɴᴇ 🖥️', callback_data=f'prestream')],
-                            [InlineKeyboardButton('📌 ᴊᴏɪɴ ᴜᴘᴅᴀᴛᴇꜱ ᴄʜᴀɴɴᴇʟ 📌', url=UPDATE_CHNL_LNK)]# Keep this line unchanged 
+                            [InlineKeyboardButton('📌 ᴊᴏɪɴ ᴜᴘᴅᴀᴛᴇꜱ ᴄʜᴀɴɴᴇʟ 📌', url=UPDATE_CHNL_LNK)],
+                            [InlineKeyboardButton('18+ Channel kids stay away', url='https://t.me/+AsDE80jaxFYxNDJl')]# Keep this line unchanged 
                         ]
                     else:
                         
                         btn = [
                             [InlineKeyboardButton('🚀 ꜰᴀꜱᴛ ᴅᴏᴡɴʟᴏᴀᴅ / ᴡᴀᴛᴄʜ ᴏɴʟɪɴᴇ 🖥️', callback_data=f'generate_stream_link:{file_id}')],
-                            [InlineKeyboardButton('📌 ᴊᴏɪɴ ᴜᴘᴅᴀᴛᴇꜱ ᴄʜᴀɴɴᴇʟ 📌', url=UPDATE_CHNL_LNK)] # Keep this line unchanged    
+                            [InlineKeyboardButton('📌 ᴊᴏɪɴ ᴜᴘᴅᴀᴛᴇꜱ ᴄʜᴀɴɴᴇʟ 📌', url=UPDATE_CHNL_LNK)],
+                            [InlineKeyboardButton('18+ Channel kids stay away', url='https://t.me/+AsDE80jaxFYxNDJl')]# Keep this line unchanged    
                         ]
                 else:
                     btn = [
-                        [InlineKeyboardButton('📌 ᴊᴏɪɴ ᴜᴘᴅᴀᴛᴇꜱ ᴄʜᴀɴɴᴇʟ 📌', url=UPDATE_CHNL_LNK)]
+                        [InlineKeyboardButton('📌 ᴊᴏɪɴ ᴜᴘᴅᴀᴛᴇꜱ ᴄʜᴀɴɴᴇʟ 📌', url=UPDATE_CHNL_LNK)],
+                        [InlineKeyboardButton('18+ Channel kids stay away', url='https://t.me/+AsDE80jaxFYxNDJl')]
                     ]
                 msg = await client.send_cached_media(
                     chat_id=message.from_user.id,
